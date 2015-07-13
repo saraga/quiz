8e96748c999958eeadfaba22adf76cf580a0a45a
saraga <sara.garrote@gmail.com>
2015-07-13 13:06:58 +0200

    Ejercicio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,8 +6,13 @@ exports.question = function(req, res) {
 // GET /quizers/answer
 exports.answer = function(req, res) {
 	if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', {pregunta: 'Corecto'});
-	} else {}
-		res.render('quizes/answer', {pregunta: 'Incorrecto'});
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
-};
+}
+
+// GET /quizers/author
+exports.author = function(req, res) {
+	res.render('quizes/author');
+}
